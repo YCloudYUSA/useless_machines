66aec816c49180c55aae376a741756b6ec528bb2
svicervlad <vlad.sadretdinov@jet.dev>
2024-04-03 10:13:36 +0000

    fix: jquery_ui_draggable no longer in distro

---
+++ jquery_ui_draggable/jquery_ui_draggable.post_update.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Post update functions for the jquery_ui_draggable useless machine.
+ */
+
+/**
+ * Uninstall me.
+ */
+function jquery_ui_draggable_reset_post_update_uninstall() {
+  \Drupal::service('module_installer')->uninstall(['jquery_ui_draggable']);
+}
